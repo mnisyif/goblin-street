6ffe7bfcd69128f76f355e1666ad8be290a9f5a1
Murtadha <mnisyif@gmail.com>
2026-07-13 18:36:28 +0300

    feature: add goblincache package and Cache struct to hold cache entries

---
+++ internal/goblincache/goblincache.go
@@ -0,0 +1,39 @@
+// Copyright 2026 mnisyif
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package goblincache is for caching requests from OSRS wiki prices api
+package goblincache
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type Cache struct {
+	cacheMap map[string]cacheEntry
+	mu       *sync.Mutex
+}
+
+func New(interval time.Duration) (*Cache, error) {
+	newCache := &Cache{
+		cacheMap: make(map[string]cacheEntry),
+		mu:       &sync.Mutex{},
+	}
+	return newCache, nil
+}
